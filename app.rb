75f31055859827ef90a74f370b88eb434de0c2c9
Jonathan Pichot <jonathan@pichot.us>
2015-02-04 18:13:04 -0500

    Add Foursquare endpoint.

--- app.rb
+++ app.rb
@@ -4,11 +4,6 @@ require "bundler"
 Bundler.require
 Dotenv.load
 
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = __dir__
-end
-
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 module Mantle
@@ -18,6 +13,13 @@ module Mantle
 
       page = FacebookPage.new(params[:id])
       page.attributes.to_json
+    end
+
+    get "/location/foursquare/:id" do
+      content_type :json
+
+      venue = FoursquareVenue.new(params[:id])
+      venue.attributes.to_json
     end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,35 +10,47 @@ RSpec.describe "Mantle" do
         response = JSON.parse(last_response.body)
 
         expect(response).to eq({
-          "address"=>"235 Grandville Ave SW",
-          "latitude"=>42.958605,
-          "longitude"=>-85.673631,
-          "phone"=>"(616) 776-2182",
-          "source_link"=>"https://www.facebook.com/foundersbrewing",
-          "website"=>"www.foundersbrewing.com",
-          "hours"=>nil,
-          "price_range"=>nil,
-          "tags"=>["Brewery"]
+          "address" => "235 Grandville Ave SW",
+          "cash_only" => nil,
+          "delivery" => nil,
+          "hours" => nil,
+          "kids" => nil,
+          "latitude" => 42.958605,
+          "longitude" => -85.673631,
+          "outdoor" => nil,
+          "phone" => "(616) 776-2182",
+          "price_range" => nil,
+          "reserve" => nil,
+          "source_link" => "https://www.facebook.com/foundersbrewing",
+          "tags" => ["Brewery"],
+          "takeout" => nil,
+          "website" => "www.foundersbrewing.com",
         })
       end
     end
 
     describe "from Foursquare", vcr: {cassette_name: "location_foursquare"}  do
       it "returns a location json object" do
-        get "/location/foursquare/founderbrewing"
+        get "/location/foursquare/4b12c269f964a5208b8d23e3"
 
         response = JSON.parse(last_response.body)
 
         expect(response).to eq({
           "address"=>"235 Grandville Ave SW",
-          "latitude"=>42.958605,
-          "longitude"=>-85.673631,
-          "phone"=>"(616) 776-2182",
-          "source_link"=>"https://www.facebook.com/foundersbrewing",
-          "website"=>"www.foundersbrewing.com",
-          "hours"=>nil,
-          "price_range"=>nil,
-          "tags"=>["Brewery"]
+          "cash_only" => false,
+          "delivery" => false,
+          "hours" => "Mon–Sat 11:00 AM–2:00 AM Sun Noon–Midnight",
+          "kids" => nil,
+          "latitude" => 42.958428066366515,
+          "longitude" => -85.6737289899219,
+          "outdoor" => true,
+          "phone" => "(616) 776-1195",
+          "price_range" => "$$",
+          "reserve" => false,
+          "source_link" => "https://foursquare.com/v/founders-brewing-co/4b12c269f964a5208b8d23e3",
+          "tags" => ["Brunch", "Lunch", "Dinner", "Beer", "Wine", "Brewery", "Sandwiches"],
+          "takeout" => false,
+          "website" => "http://www.foundersbrewing.com",
         })
       end
     end
