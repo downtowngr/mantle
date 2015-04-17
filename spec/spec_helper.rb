02bf92c110450e0fc2e400e4545b1fe71c607329
Jonathan Pichot <jonathan@pichot.us>
2015-04-17 14:20:17 -0400

    Ignore Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,7 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.default_cassette_options = { match_requests_on: [:host, :path] }
+  c.ignore_hosts "codeclimate.com"
 end
 
 RSpec.configure do |config|
