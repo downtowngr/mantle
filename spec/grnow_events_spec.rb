cb7ca43accd1edad6ee49357e8cc4503090ecaf7
Jonathan Pichot <jonathan@pichot.us>
2015-10-12 12:41:48 +0200

    GRNow now responds with UTC times, requiring less backflips to get the correct times.

--- spec/grnow_events_spec.rb
+++ spec/grnow_events_spec.rb
@@ -9,27 +9,56 @@ RSpec.describe GrnowEvents do
       expect(events).to be_a Hash
     end
 
-    describe "attributes" do
-      let(:event) { events[:events].first }
+    context "all day event" do
+      describe "attributes" do
+        let(:event) { events[:events][0] }
 
-      it "event_name" do
-        expect(event[:event_name]).to eq("Joe Bonamassa at DeVos Performance Hall")
+        it "event_name" do
+          expect(event[:event_name]).to eq("Grand Rapids Ballet 2015-16 Season Lineup")
+        end
+
+        it "start_time" do
+          # Midnight EDT.
+          expect(event[:start_time]).to eq(1444363200)
+        end
+
+        it "end_time" do
+          expect(event[:end_time]).to be_nil
+        end
+
+        it "external_id" do
+          expect(event[:external_id]).to eq("29955-1444363200-1444449599@www.grnow.com")
+        end
+
+        it "event_url" do
+          expect(event[:event_url]).to eq("http://www.grnow.com/event/grand-rapids-ballet-2015-16-season-lineup/")
+        end
       end
+    end
 
-      it "start_time" do
-        expect(event[:start_time]).to eq(1429056000)
-      end
+    context "timed event" do
+      describe "attributes" do
+        let(:event) { events[:events][1] }
 
-      it "end_time" do
-        expect(event[:end_time]).to be_nil
-      end
+        it "event_name" do
+          expect(event[:event_name]).to eq("Grand Rapids Symphony: Star Trek Live in Concert")
+        end
 
-      it "external_id" do
-        expect(event[:external_id]).to eq("10676-1429041600-1429041600@http://www.grnow.com")
-      end
+        it "start_time" do
+          expect(event[:start_time]).to eq(1445126400)
+        end
 
-      it "event_url" do
-        expect(event[:event_url]).to eq("http://www.grnow.com/event/joe-bonamassa-at-devos-performance-hall/")
+        it "end_time" do
+          expect(event[:end_time]).to eq(1445137200)
+        end
+
+        it "external_id" do
+          expect(event[:external_id]).to eq("35495-1445126400-1445137200@www.grnow.com")
+        end
+
+        it "event_url" do
+          expect(event[:event_url]).to eq("http://www.grnow.com/event/grand-rapids-symphony-star-trek-live-concert/")
+        end
       end
     end
   end
